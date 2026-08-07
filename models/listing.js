7b622dc7e61b37d720e70dbd8929f7dabb0ae4ce
piyush <piyushshende112@gmail.com>
2026-08-08 00:29:34 +0530

    feat: implement database schemas and initialization scripts

---
+++ models/listing.js
@@ -0,0 +1,29 @@
+const mongoose=require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type:String,
+        required:true,
+    },
+    description:String,
+    image:{
+        filename:String,
+        url:{
+            type:String,
+            default:
+            "https://unsplash.com/photos/vs-eYSV5lMU",
+        set:(v)=>
+            v===""
+                ?"https://unsplash.com/photos/vs-eYSV5lMU"
+                :v,
+    }
+},
+    price:Number,
+    location:String,
+    country:String,
+});
+
+const Listing=mongoose.model("Listing",listingSchema);
+
+module.exports=Listing;
